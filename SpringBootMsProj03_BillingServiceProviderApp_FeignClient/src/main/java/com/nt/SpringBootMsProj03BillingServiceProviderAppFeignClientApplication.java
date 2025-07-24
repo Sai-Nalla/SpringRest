44d5f3354266509dfa6873aae3d3ee0f37d5b58c
sainalla <sainalla@localhost>
2025-07-24 15:46:35 -0500

    Project3

---
+++ SpringBootMsProj03_BillingServiceProviderApp_FeignClient/src/main/java/com/nt/SpringBootMsProj03BillingServiceProviderAppFeignClientApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMsProj03BillingServiceProviderAppFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMsProj03BillingServiceProviderAppFeignClientApplication.class, args);
+	}
+
+}
