8fdac3462522145923e3df5c44656317407b1bba
sainalla <sainalla@localhost>
2025-08-07 23:13:17 -0500

    JMS  Point to point Commuicatiion

---
+++ BootJMSProj1_ProducerApp/src/main/java/com/nt/sender/ActiveMQMessageSender.java
@@ -0,0 +1,44 @@
+package com.nt.sender;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.Session;
+
+@Component
+public class ActiveMQMessageSender {
+	
+	@Autowired
+	private JmsTemplate template;
+
+	@Scheduled(cron = "*/10 * * * * *")
+	public void send() {
+		// TODO Auto-generated method stub
+		/*template.send("testmQ1", new MessageCreator() {
+			
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				// TODO Auto-generated method stub
+				return session.createTextMessage("From Sender "+new Date());
+			}
+		});*/
+
+		// using Lamda Style inner class
+		
+		template.send("testmQ1",ses->{
+			return ses.createTextMessage("From Sender "+new Date());
+			
+		}
+		);
+		System.out.println("Message Sent");
+	}
+
+}
---
+++ BootJMSProj1_RecieverApp-1/src/main/java/com/nt/BootJmsProj1RecieverApp1Application.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootJmsProj1RecieverApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootJmsProj1RecieverApp1Application.class, args);
+	}
+
+}
